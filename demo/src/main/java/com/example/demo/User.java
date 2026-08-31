97d8e25d1d476c96e570ee1f3387a13df6e7e712
SiliconPulse50 <3755622927@qq.com>
2026-08-31 21:48:04 +0800

    新增password字段

--- demo/src/main/java/com/example/demo/User.java
+++ demo/src/main/java/com/example/demo/User.java
@@ -21,15 +21,28 @@ private String username;
 @NotNull(message="手机号不能为空")
 @Pattern(regexp="^1[3-9]\\d{9}$", message = "手机号格式不正确")
 //意思是“1开头，第二位是3-9，后面跟9个数字”，总共11位。
-    private String phone;
+private String phone;
+private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     // 无参构造（必须要有）
     public User() {}
 
     // 有参构造（方便测试）
-    public User(Long id, String username, String phone) {
+
+
+    public User(Long id, String username, String phone, String password) {
         this.id = id;
         this.username = username;
         this.phone = phone;
+        this.password = password;
     }
 
     // 右键 -> Generate -> Getter and Setter -> 全选生成
